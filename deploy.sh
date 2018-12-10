a5a33ce748721fc88b7a357560ecfd9a8ea43490
Dmytro <dmytro@dzubenko.name>
2018-12-10 13:49:38 +0200

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ set -ex
 
 git config user.name "Travis Deploy"
 git config user.email "deploy@travis-ci.org"
-git remote add gh-token "https://${GH_TOKEN}@github.com/paycoreio/docs.git";
-git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
-mkdocs gh-deploy -v --clean --remote-name gh-token;
+git remote add gh-token "https://${GH_TOKEN}@github.com/paycoreio/docs.git"
+git fetch gh-token && git fetch gh-token gh-pages:gh-pages
+mkdocs gh-deploy -v --clean --remote-name gh-token
